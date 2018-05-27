339499f28f94f3b95a0514ea2e31f2f89a5f6aab
RomanLevkovych <rlevkovych098@gmail.com>
2018-05-27 21:43:06 +0300

    I haven't noticed that I pushed empty file to the task 4.

--- Task4/main.cpp
+++ Task4/main.cpp
@@ -1,8 +1,21 @@
 #include <QCoreApplication>
+#include <QTimer>
+#include <QDebug>
+#include <QString>
+
+void printFinish()
+{
+    qDebug() << "finished\n";
+    QCoreApplication::quit();
+}
 
 int main(int argc, char *argv[])
 {
+    const int second2msecondConvert = 1000;
     QCoreApplication a(argc, argv);
+    int timeout = a.arguments().at(1).toInt();
+    qDebug() << "Start: \n";
+    QTimer::singleShot(timeout*second2msecondConvert, &a, &printFinish);
 
     return a.exec();
 }
