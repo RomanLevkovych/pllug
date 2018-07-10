9c857f83ec7a5e55f9cc894d5311a7af75ab3664
RomanLevkovych <rlevkovych098@gmail.com>
2018-07-10 12:09:07 +0300

    Small fix task 4

--- Task4/main.cpp
+++ Task4/main.cpp
@@ -14,6 +14,10 @@ int main(int argc, char *argv[])
     const int second2msecondConvert = 1000;
     QCoreApplication a(argc, argv);
     int timeout = a.arguments().at(1).toInt();
+    if (timeout < 0)
+    {
+        return -1;
+    }
     qDebug() << "Start: \n";
     QTimer::singleShot(timeout*second2msecondConvert, &a, &printFinish);
 
